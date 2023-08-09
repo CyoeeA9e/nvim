2c28d56c685b8358a40fa6ba1f2c99934afadae9
CyoeeA9e <z_y@hust.edu.cn>
2023-08-09 11:51:07 +0800

    nvim init

---
+++ lua/plugins/editor.lua
@@ -0,0 +1,10 @@
+return { 'tpope/vim-sleuth', {
+    -- Add indentation guides even on blank lines
+    'lukas-reineke/indent-blankline.nvim',
+    -- Enable `lukas-reineke/indent-blankline.nvim`
+    -- See `:help indent_blankline.txt`
+    opts = {
+        char = '┊',
+        show_trailing_blankline_indent = false
+    }
+} }
