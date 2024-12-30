1b80a330bb2490e4f11a93359d8a362b89f15359
CyoeeA9e <zjnbzhzy@outlook.com>
2024-12-30 14:51:25 +0800

    terminal

--- lua/custom/plugins/term.lua
+++ lua/custom/plugins/term.lua
@@ -1,1 +1,21 @@
-return { 'akinsho/toggleterm.nvim', config = true }
+local toggle_term_by_window_size = function()
+  local width = vim.o.columns
+  local height = vim.o.lines
+  if width >= height then
+    vim.cmd 'ToggleTerm direction=horizontal'
+  else
+    vim.cmd 'ToggleTerm direction=vertical'
+  end
+end
+
+return {
+  'akinsho/toggleterm.nvim',
+  opts = {
+    start_in_insert = true,
+    persist_mode = false,
+  },
+  keys = {
+    { '<C-\\><C-\\>', toggle_term_by_window_size, mode = 'n', desc = 'Toggle terminal' },
+    { '<C-\\><C-\\>', toggle_term_by_window_size, mode = 't', desc = 'Toggle terminal' },
+  },
+}
