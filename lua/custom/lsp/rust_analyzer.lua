84bc2aa2eb4b8fcd36769b3f0141753d7ed106f2
CyoeeA9e <zjnbzhzy@outlook.com>
2025-01-03 18:01:37 +0800

    rust-analyzer search

--- lua/custom/lsp/rust_analyzer.lua
+++ lua/custom/lsp/rust_analyzer.lua
@@ -1,5 +1,25 @@
+local search_rustanalyzer_settings = function()
+  local search_up
+  search_up = function(dir)
+    if dir == nil or dir == '/' then
+      return nil
+    end
+    local rust_analyzer_json = dir .. '/.rust-analyzer.json'
+    if vim.fn.filereadable(rust_analyzer_json) == 1 then
+      return rust_analyzer_json
+    end
+    return search_up(vim.fn.fnamemodify(dir, ':h'))
+  end
+  return search_up(vim.fn.getcwd())
+end
+
 local get_project_rustanalyzer_settings = function()
-  local handle = io.open(vim.fn.resolve(vim.fn.getcwd() .. '/./.rust-analyzer.json'))
+  -- local handle = io.open(vim.fn.resolve(vim.fn.getcwd() .. '/./.rust-analyzer.json'))
+  local file = search_rustanalyzer_settings()
+  if not file then
+    return {}
+  end
+  local handle = io.open(file)
   if not handle then
     return {}
   end
